c14e61c4395279c4f39889f8c36f166e9e81dff7
Kane <kanelariviere@outlook.com>
2024-06-03 20:17:54 -0400

    user scoped data

--- convex/projects.ts
+++ convex/projects.ts
@@ -1,20 +1,36 @@
 import { mutation, query } from './_generated/server'
-import { v } from 'convex/values'
+import { ConvexError, v } from 'convex/values'
 
 export const createProject = mutation({
     args: {
         title: v.string(),
-        
     },
     async handler(ctx, args) {
+
+        const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier
+
+        if(!userId) {
+            throw new ConvexError('Not authenticated!')
+        }
+        
         await ctx.db.insert('projects', {
             title: args.title,
+            tokenIdentifier: userId,
         })
     },
 })
 
 export const getProjects = query({
-    async handler(ctx) {
-        return await ctx.db.query('projects').collect()
+    async handler(ctx) { 
+
+        const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier
+
+        if(!userId) {
+            return [];
+        }
+        
+        return await ctx.db.query('projects')
+            .withIndex('by_tokenIdentifier', (q) => q.eq('tokenIdentifier', userId
+            )).collect()
     },
 });
--- convex/schema.ts
+++ convex/schema.ts
@@ -3,6 +3,7 @@ import { v } from "convex/values";
 
 export default defineSchema({
     projects: defineTable({ 
-        title: v.string() 
-    }),
+        title: v.string(),
+        tokenIdentifier: v.string(),
+    }).index('by_tokenIdentifier', ['tokenIdentifier']),
 });
