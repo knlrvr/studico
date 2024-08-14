3ac7a284f248f820c0e6d178b170f4e43900383b
Kane <kanelariviere@outlook.com>
2024-08-13 22:39:43 -0400

    delete project card display

--- src/components/settings.tsx
+++ src/components/settings.tsx
@@ -1,14 +1,32 @@
-import { useProjectId } from "@/app/dashboard/projects/context"
+import { useProjectId } from "@/app/dashboard/projects/context";
 import { DeleteProject } from "./deleteProject";
 import Members from "./members";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
 
+import { useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
+
+import { 
+    Card, 
+    CardContent, 
+    CardDescription, 
+    CardHeader, 
+    CardTitle 
+} from "./ui/card";
+import { useUser } from "@clerk/nextjs";
 
 export default function SettingsContent() {
 
+    const projectId = useProjectId();
+    const { user } = useUser();
+
+    const currentProject = useQuery(api.projects.getProject, {
+        projectId: projectId,
+    });
     return (
         <div className="space-y-4">
             <Members />
+
+            {currentProject?.tokenIdentifier?.includes(user?.id as string) && (
             <Card>
                 <CardHeader>
                     <CardTitle>Delete Project</CardTitle>
@@ -18,11 +36,13 @@ export default function SettingsContent() {
                         absolutely sure that you want to delete this project.
                   </CardDescription>
                 </CardHeader>
+
                 <CardContent>
                     <DeleteProject />
                 </CardContent>
-                </Card>
-
+            </Card>
+            )}
+            
         </div>
     )
 }
