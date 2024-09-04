96c7f4c5dac980bd4d5dff47dd449f38c62f54ff
Kane <kanelariviere@outlook.com>
2024-09-04 13:56:00 -0400

    this is a mistake

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressContentEditableWarning suppressHydrationWarning>
       <body className={cn(
         "min-h-screen bg-background font-sans antialiased",
         fontSans.variable
